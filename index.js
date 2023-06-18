4f448012ef84f4439c129d96535f61b446d7c35d
Sandeep <sandeepsingh798257@gmail.com>
2023-06-18 12:43:47 +0530

    Converting to Ajax

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
 const MongoStore = require ('connect-mongo');
+const flash= require('connect-flash');
+const customMware= require('./config/middleware');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -56,6 +58,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
 
 //use express router
 app.use('/', require('./routes'));
